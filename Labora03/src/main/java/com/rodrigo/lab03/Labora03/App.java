0e105f79df866ac2838533f9df2e4baf0b0698e8
Rodrigo <exit>
2017-08-29 09:47:06 -0500

    fd

---
+++ Labora03/src/main/java/com/rodrigo/lab03/Labora03/App.java
@@ -0,0 +1,13 @@
+package com.rodrigo.lab03.Labora03;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
